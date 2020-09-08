86cc4183ed488fec444b4f8304fab379990a4253
repl.it user <replituser@example.com>
2020-09-08 11:39:47 +0000

    fix

--- jmg/blog/tests.py
+++ jmg/blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from blog.models import Post
 
-# Create your tests here.
+class PostTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username='admin', password='admin')
+
+    def test_is_published_return_false(self):
+        post = Post.objects.create(author=user, title='mi titulo')
+
+        is_published = post.is_published()
+
+        self.assertFalse(is_published)
--- jmg/jmg/settings.py
+++ jmg/jmg/settings.py
@@ -58,7 +58,9 @@ ROOT_URLCONF = 'jmg.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'template'
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
